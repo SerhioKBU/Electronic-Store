eab1527ca8ca455fd0c1659e0c05433b4487ac18
SerhioKBU <chebanovserhii@gmail.com>
2021-12-01 20:59:38 +0200

    Homeworks 18, 20

--- src/main/resources/Data base/electronic-store_init.sql
+++ src/main/resources/Data base/electronic-store_init.sql
@@ -1,15 +1,19 @@
-Insert Into users(UserName, Password, Email) value ('Tommy', '2017', 'tom@mail.ua');
-Insert Into users(UserName, Password, Email) value ('Koko', '1234', 'coco@gmail.com');
-Insert Into users(UserName, Password, Email) value ('Serhii', 'qwerty', 'serhii_2021@gmail.com');
+# SET FOREIGN_KEY_CHECKS=0;
+# SET FOREIGN_KEY_CHECKS=1;
+
+Insert Into account(login, password) value ('Serhio1982', 'qwerty');
+Insert Into account(login, password) value ('Batman', '1234');
+
+Insert Into users(accountId, UserName, Email) value (10, 'Sergey', 'serhii_2021@meta.ua');
+Insert Into users(accountId, UserName, Email) value ((SELECT `id` FROM `account` WHERE `login` = 'Batman'), 'Olya', 'ola@meta.ua');
+
+Insert Into category(name) value ('Mobiles phone');
+Insert Into category(name) value ('TV');
+Insert Into category(name) value ('Laptop');
+Insert Into category(name) value ('Toy');
 
 Insert Into product(CategoryId, name, cost) value (2, 'Samsung', 750);
 Insert Into product(CategoryId, name, cost) value (2, 'Philips', 480);
 
 Insert Into basket(UserId, ProductId) value (1, 1);
 Insert Into basket(UserId, ProductId) value (1, 2);
-
-Insert Into category(name) value ('Mobiles phone');
-Insert Into category(name) value ('TV');
-Insert Into category(name) value ('Laptops');
-Insert Into category(name) value ('Toy');
-
